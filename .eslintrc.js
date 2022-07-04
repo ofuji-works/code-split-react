2d6e37bf70d4ec2624d6010852431e2772379ff3
ofuji-works <ofuji.works@gmail.com>
2022-07-04 14:00:04 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2021,
+        sourceType: 'module'
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-hooks',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ],
+}
